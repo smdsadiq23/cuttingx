321d439b3d4afe04cf3a20cf4bd3460a9052f78d
Sathish Kumar V <sathish@cognitionx.tech>
2025-07-29 21:36:34 +0530

    Bundle Creation Doctype

---
+++ cuttingx/cuttingx/doctype/bundle_creation/bundle_creation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Cognitonx Logic India Private limited and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Bundle Creation", {
+// 	refresh(frm) {
+
+// 	},
+// });
