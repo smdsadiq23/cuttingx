180770b458bb2095139cca9fba5f477f568bdf8b
Sathish Kumar V <sathish@cognitionx.tech>
2025-10-17 12:36:14 +0530

    Update sampling_status.py

--- cuttingx/cuttingx/doctype/sampling_status/sampling_status.py
+++ cuttingx/cuttingx/doctype/sampling_status/sampling_status.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2025, Cognitonx Logic India Private limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.utils import flt
 from frappe.model.document import Document
 
 
 class SamplingStatus(Document):
-	pass
+	def before_save(self):
+		total = sum(flt(row.weight) for row in self.table_sampling_status_consumption)
+		self.total_consumption_weight = total
+
+		for row in self.table_sampling_status_consumption:
+			row.percentage = (flt(row.weight) / total * 100) if total else 0
